4fc313812730b2ebde7f7b17efed01b79188ebe2
Adhamamer-V <142250985+Adhamamer-V@users.noreply.github.com>
2023-10-10 02:21:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,51 +11,56 @@ fetch("data/data.json")
   });
 const countryCards = document.getElementById("country-cards");
 const input = document.getElementById("search-input");
-const continentSelect = document.getElementById("continent-select");
 const searchIcon = document.querySelector(".searchIcon");
 
 function filterCountries() {
   const searchTerm = input.value.toLowerCase();
-  const selectedContinent = continentSelect.value.toLowerCase();
-  const filteredCountries = allCountries.filter((country) => {
-    const nameMatch = country.name.toLowerCase().includes(searchTerm);
-    const continentMatch =
-      selectedContinent === "" ||
-      country.region.toLowerCase() === selectedContinent;
-    return nameMatch && continentMatch;
-  });
+  const filteredCountries = allCountries.filter((country) =>
+    country.name.toLowerCase().includes(searchTerm)
+  );
+
   displayCountries(filteredCountries);
 }
 
 function displayCountries(countries) {
   countryCards.innerHTML = "";
+
   countries.forEach((country) => {
     const card = document.createElement("div");
     card.classList.add("card");
+
     const flag = document.createElement("img");
     flag.src = country.flags.png;
     flag.alt = `${country.name} flag`;
     card.appendChild(flag);
+
     const textCard = document.createElement("div");
     textCard.classList.add("Text-Card");
     card.appendChild(textCard);
+
     const countryName = document.createElement("h2");
     countryName.textContent = country.name;
     textCard.appendChild(countryName);
+
     const population = document.createElement("p");
     population.textContent = `Population: ${country.population}`;
     textCard.appendChild(population);
+
     const region = document.createElement("p");
     region.textContent = `Region: ${country.region}`;
     textCard.appendChild(region);
+
     const capital = document.createElement("p");
     capital.textContent = `Capital: ${country.capital}`;
     textCard.appendChild(capital);
+
     card.addEventListener("click", () => {
       window.open(`country-details.html?name=${country.name}`, "_blank");
     });
+
     countryCards.appendChild(card);
   });
+
   if (countries.length === 0) {
     const noResults = document.createElement("p");
     noResults.textContent = "No matching countries found.";
@@ -69,4 +74,3 @@ function performSearch() {
 
 input.addEventListener("input", performSearch);
 searchIcon.addEventListener("click", performSearch);
-continentSelect.addEventListener("change", filterCountries);
